129205f8b4299c329fc08ada7ea13973af33af62
Ficoto <438500829@qq.com>
2020-08-31 17:59:34 +0800

    feat: pprof(#3)

--- go.mod
+++ go.mod
@@ -3,9 +3,9 @@ module github.com/xiaojiaoyu100/lizard
 go 1.14
 
 require (
+	github.com/gin-gonic/gin v1.6.3
 	github.com/go-redis/redis/v7 v7.2.0
 	github.com/onsi/ginkgo v1.10.2 // indirect
 	golang.org/x/net v0.0.0-20200226121028-0de0cce0169b // indirect
-	golang.org/x/sys v0.0.0-20191026070338-33540a1f6037 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 )
---
+++ pprof/matryoshka.go
@@ -0,0 +1,54 @@
+package pprof
+
+import (
+	"net"
+	"net/http"
+	"net/http/pprof"
+
+	"github.com/gin-gonic/gin"
+)
+
+func isInternalIP(ip string) bool {
+	i := net.ParseIP(ip)
+	if i.IsLoopback() {
+		return true
+	}
+	if ip4 := i.To4(); ip4 != nil {
+		switch {
+		case ip4[0] == 10:
+			return true
+		case ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31:
+			return true
+		case ip4[0] == 192 && ip4[1] == 168:
+			return true
+		default:
+			return false
+		}
+	}
+	return false
+}
+
+func auth(fn func(w http.ResponseWriter, r *http.Request)) func(c *gin.Context) {
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+		if !isInternalIP(ip) {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		fn(c.Writer, c.Request)
+	}
+}
+
+func InitRoutes(engine *gin.Engine) {
+	engine.GET("/debug/pprof/", auth(pprof.Index))
+	engine.GET("/debug/pprof/cmdline", auth(pprof.Cmdline))
+	engine.GET("/debug/pprof/profile", auth(pprof.Profile))
+	engine.GET("/debug/pprof/symbol", auth(pprof.Symbol))
+	engine.GET("/debug/pprof/trace", auth(pprof.Trace))
+	engine.GET("/debug/pprof/goroutine", auth(pprof.Index))
+	engine.GET("/debug/pprof/allocs", auth(pprof.Index))
+	engine.GET("/debug/pprof/block", auth(pprof.Index))
+	engine.GET("/debug/pprof/heap", auth(pprof.Index))
+	engine.GET("/debug/pprof/mutex", auth(pprof.Index))
+	engine.GET("/debug/pprof/threadcreate", auth(pprof.Index))
+}
