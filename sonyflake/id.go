1f5b03419ecda8976779124b43904b86274f55e0
Lincoln_Lee <138951227@qq.com>
2020-11-05 10:51:01 +0800

    feat: ID
    * feat: add sonyflake
    
    * feat: add type ID
    
    * feat: update function name

---
+++ sonyflake/id.go
@@ -0,0 +1,27 @@
+package sonyflake
+
+import "strconv"
+
+type ID int64
+
+func (i *ID) MarshalJSON() ([]byte, error) {
+	b := []byte(strconv.FormatInt(int64(*i), 10))
+	return b, nil
+}
+
+func (i *ID) UnmarshalJSON(b []byte) error {
+	id, err := strconv.ParseInt(string(b), 10, 64)
+	if err != nil {
+		return err
+	}
+	*i = ID(id)
+	return nil
+}
+
+func (i *ID) Int64() int64 {
+	return int64(*i)
+}
+
+func (i *ID) String() string {
+	return strconv.FormatInt(int64(*i), 10)
+}
